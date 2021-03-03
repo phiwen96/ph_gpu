7eb56ecab17dbc9cde8ebde0bdeb1bf11188a545
phiwen96 <philip.sve@hotmail.com>
2021-03-03 15:23:43 +0100

    a

---
+++ tests/testlib.cpp
@@ -0,0 +1,15 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include <ph_gpu/ph_gpu.hpp>
+using namespace std;
+
+
+TEST_CASE( "Quick check", "[kiss]" ) {
+//      cout << GPU_0_MAX_IMAGE_DIMENSION_1D << endl;
+//       cout << gpu<0>::max_image_dimension_1D << endl;
+//       cout << kuk << endl;
+      cout << "hi" << endl;
+
+// 	std::cout << "hi" << std::endl;
+//     REQUIRE( kiss () == 0 );
+}
